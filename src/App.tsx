5e71a78d2e5fa6034278e7b3847315b43ca90bc5
ericayoung <eyoung135@gmail.com>
2023-08-31 02:02:52 -0400

    finished creating the body and win/lose with visibility and hidden functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import { useState } from "react"
 import words from "./wordList.json"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { Keyboard } from "./Keyboard"
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
-  console.log(wordToGuess)
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
-  return <h1> Hello World </h1>
+  return <div 
+  style = {{
+    maxWidth: "880px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    margin: "0 auto",
+    alignItems: "center",
+  }}>
+    <div 
+      style={{
+        fontSize: "2rem",textAlign: "center"
+      }}>
+        Lose Win
+    </div>
+    <HangmanDrawing />
+    <HangmanWord />
+    <div style={{ alignSelf: "stretch"}}>
+    <Keyboard />
+    </div>
+  </div>
 }
 
 export default App
